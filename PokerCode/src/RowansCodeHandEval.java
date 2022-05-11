7d446cdc3b160065eb9911289ab77c90912beac1
ewaite135 <ewaite135@gmail.com>
2022-05-11 10:05:58 -0700

    added rowan's code

--- PokerCode/src/RowansCodeHandEval.java
+++ PokerCode/src/RowansCodeHandEval.java
@@ -1,41 +1,46 @@
-public class RowansCodeHandEval {
+public class RowansCodeHandEval<i> {
 
     // handValues is an array of the number values of each card in all player's hands, from P1 card 1, P1 card 2, P2 card 1, etc.
     // commValues is an array of the number values of the community cards.
     // playerScore is an array of the score of each player, in points used only for determining the winner.
-
-    for (int i = 0; i < handValues.length; i+=2) {          // Checks the pairs in all hands and community cards.
-        int max = commValues.length;
-        int copies = 0;
-        for (int j = 0; j < max; j++) {
-            if (handValues[i] == commValues[j]) {
-                copies++;
-                playerScore[i/2] = (100 * (10^copies) + (handValues[i] * 10));
-            }
-        }
-        if (handValues[i] == handValues [i + 1]) {
-            copies++;
-            playerScore[i/2] = (100 * (10^copies) + (handValues[i] * 10));
-        }
-
-        if (handValues[i] != handValues[i + 1]) {        // Repeats checking the community cards if hand doesn't contain same cards
-            copies = 0;
-            int tempTotal = 0;
-            for (int j = 0; j < max; j++) {
-                if (handValues[i + 1] == commValues[j]) {
+    //Just one persons hand
+    private int[] handValues = {0, 0};
+    //The board
+    private int[] commValues = {0, 0, 0, 0, 0};
+    private int playerScore = 0;
+    public int getEvaluation() {
+        for (int cardInHand = 0; cardInHand < 2; cardInHand++) {          // Checks the pairs in all hands and community cards.
+            int numCommCards = commValues.length;
+            int copies = 0;
+            for (int j = 0; j < numCommCards; j++) { //
+                if (handValues[cardInHand] == commValues[j]) {
                     copies++;
-                    tempTotal = (100 * (10^copies) + (handValues[i + 1] * 10));
+                    playerScore = (100 * (10 ^ copies) + (handValues[cardInHand] * 10));
                 }
             }
-            playerScore[i/2] += tempTotal;
-        }
-        if (playerScore[i/2] == 0) {
-            if (handValues[i] >= handValues [i + 1]) {
-                playerScore[i/2] += handValues[i];
-            } else {
-                playerScore[i/2] += handValues[i + 1];
+            if (handValues[cardInHand] == handValues[cardInHand + 1]) { //If your hand contains a pair
+                copies++;
+                playerScore = (100 * (10 ^ copies) + (handValues[cardInHand] * 10));
+            } else {        // Repeats checking the community cards if hand doesn't contain a pair
+                copies = 0;
+                int tempTotal = 0;
+                for (int j = 0; j < numCommCards; j++) {
+                    if (handValues[cardInHand + 1] == commValues[j]) {
+                        copies++;
+                        tempTotal = (100 * (10 ^ copies) + (handValues[cardInHand + 1] * 10));
+                    }
+                }
+                playerScore += tempTotal;
             }
-        }
+            //If you have zero pairs, it adds the high card value to your hand.
+            if (playerScore == 0) {
+                if (handValues[cardInHand] >= handValues[cardInHand + 1]) {
+                    playerScore += handValues[cardInHand];
+                } else {
+                    playerScore += handValues[cardInHand + 1];
+                }
+            }
 
+        }
     }
 }
